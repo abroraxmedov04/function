0f5da744b71cade49bbceed139e1c643087c67fb
abroraxmedov04 <abrordevaxmedov@gmail.com>
2023-10-16 17:56:32 +0300

    first-commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// function myFunc(name, surname, birthDate, job, hobby) {
+//   console.log(
+//     `My name is ${name} and surname ${surname}. 
+//     I was born at ${birthDate}.I work as a ${job},
+//      because I love to do ${hobby}.`
+//   );
+// }
+// myFunc("Abror", "Axmedov", "2004", "engineer", "codding");
+
+
+// const num1 = function(a , b ) {
+//     return (a - b);
+// }
+// console.log(`The sum of two numbers: ${num1(5, 1)}`);
+
+// const num2 = function(a , b ) {
+//     return (a + b);
+// }
+// console.log(`The sum of two numbers: ${num2(16, 1)}`);
+
+
+// const num3 = function(a , b ) {
+//     return (a / b);
+// }
+// console.log(`The sum of two numbers: ${num3(20, 1)}`);
+
+// const num4 = function(a , b ) {
+//     return (a % b);
+// }
+// console.log(`The sum of two numbers: ${num4(15, 13)}`);
+
+// const num5 = function(a , b ) {
+//     return (a * b);
+// }
+// console.log(`The sum of two numbers: ${num5(12, 1)}`);
+
+
+
